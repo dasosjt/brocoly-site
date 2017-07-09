f36c574ab41e59f32c90d05e1a32daa6d9c7d710
Diego Sosa <stdjca@gmail.com>
2017-07-09 10:21:05 -0600

    changed some className in the home page and added a cool description

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,9 @@ import React from 'react';
 class Home extends React.Component{
   render(){
     return (
-      <div>
-        Welcome Home!
+      <div className="caption center-align">
+        <h3 className="white-text"> ¡Welcome! </h3>
+			  <h5 className="light grey-text text-lighten-3"> We are ready to develop your idea.</h5>
       </div>
     )
   };
